3860052596ebf6696297d1a700f4e5e8c6402dc6
David Iglesias Zavala <david.iglesias@medinet.es>
2014-11-20 12:47:44 +0100

    fix templates

--- app/templates/_element.js
+++ app/templates/_element.js
@@ -1,11 +1,11 @@
 Polymer({
   // define element prototype here
   owner: "Daniel",
-  
+
   ready: function() {
       this.$.el.textContent = this.owner + " is ready!";
   },
-  
+
   onupdate: function() {
       //... in this Event your polymer must update your data.
   }
